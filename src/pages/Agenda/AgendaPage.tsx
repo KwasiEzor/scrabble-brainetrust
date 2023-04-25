9c50de7d287cfc9a57ef8ef7ac319a2acc8c6974
Ezor Kwasi <kwasiezor@gmail.com>
2023-04-25 10:26:34 +0200

    completing Agenda and Scrabble pages

--- src/pages/Agenda/AgendaPage.tsx
+++ src/pages/Agenda/AgendaPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeadingTitle from "../../components/HeadingTitle/HeadingTitle";
-
+import { Pagination, Table, TextInput } from "flowbite-react";
+import { HiSearch } from "react-icons/hi";
 type AgendaPageProps = {
   agendas?: [];
 };
@@ -8,7 +9,7 @@ type AgendaPageProps = {
 const AgendaPage: React.FC<AgendaPageProps> = ({ agendas }) => {
   return (
     <div>
-      <section></section>
+      {/* <Banner /> */}
       <section className="p-4">
         <HeadingTitle
           title="Agenda"
@@ -18,6 +19,110 @@ const AgendaPage: React.FC<AgendaPageProps> = ({ agendas }) => {
           subtitleClass="mt-4"
         />
       </section>
+      <section>
+        <form className="max-w-4xl mx-auto p-4 flex items-center justify-center mb-8">
+          <TextInput
+            id="searchInput"
+            rightIcon={HiSearch}
+            name="search"
+            type="search"
+            sizing="md"
+            className="w-full"
+          />
+        </form>
+      </section>
+      <article className="max-w-7xl mx-auto">
+        <Table>
+          <Table.Head>
+            <Table.HeadCell>Compétitions</Table.HeadCell>
+            <Table.HeadCell>Heures</Table.HeadCell>
+            <Table.HeadCell>Détails</Table.HeadCell>
+            <Table.HeadCell>Lieu</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                Tournoi du Scrabblophage
+                <p className="text-yellow-500 text-sm">15 Avril 2023</p>
+              </Table.Cell>
+              <Table.Cell>
+                <p className="text-xs text-center font-semibold bg-yellow-100 text-yellow-700 rounded-xl py-1">
+                  14h30
+                </p>
+              </Table.Cell>
+              <Table.Cell>
+                <p>2 parties en 3min</p>
+              </Table.Cell>
+              <Table.Cell>
+                <p>A déterminer</p>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                Amicale
+                <p className="text-blue-500 text-sm">19 Avril 2023</p>
+              </Table.Cell>
+              <Table.Cell>
+                <p className="text-xs text-center font-semibold bg-blue-100 text-blue-700 rounded-xl py-1">
+                  20h00
+                </p>
+              </Table.Cell>
+              <Table.Cell>1 partie en 3min</Table.Cell>
+              <Table.Cell>Ophain, Salle communale</Table.Cell>
+            </Table.Row>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                Interclub Bra B - Mat B
+                <p className="text-green-500 text-sm">15 Avril 2023</p>
+              </Table.Cell>
+              <Table.Cell>
+                <p className="text-xs text-center font-semibold bg-green-100 text-green-700 rounded-xl py-1">
+                  20h00
+                </p>
+              </Table.Cell>
+              <Table.Cell>Interclub + Amicale</Table.Cell>
+              <Table.Cell>Ophain, Salle communale</Table.Cell>
+            </Table.Row>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                Interclub Yod A - Bra A
+                <p className="text-green-500 text-sm">21 Avril 2023</p>
+              </Table.Cell>
+              <Table.Cell>
+                <p className="text-xs text-center font-semibold bg-green-100 text-green-700 rounded-xl py-1">
+                  20h00
+                </p>
+              </Table.Cell>
+              <Table.Cell>1 partie en 3min + Amicale</Table.Cell>
+              <Table.Cell>Trooz, Liège</Table.Cell>
+            </Table.Row>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                Simultanée mondiale
+                <p className="text-rose-500 text-sm">26 Avril 2023</p>
+              </Table.Cell>
+              <Table.Cell>
+                <p className="text-xs text-center font-semibold bg-rose-100 text-rose-700 rounded-xl py-1">
+                  20h00
+                </p>
+              </Table.Cell>
+              <Table.Cell>1 partie en 3min + Amicale</Table.Cell>
+              <Table.Cell>Ophain, Salle communale</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </article>
+      <div className="flex items-center justify-center text-center my-12">
+        <Pagination
+          currentPage={1}
+          layout="pagination"
+          onPageChange={() => null}
+          showIcons={true}
+          totalPages={1000}
+          previousLabel=""
+          nextLabel=""
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Scrabble/ScrabblePage.tsx
+++ src/pages/Scrabble/ScrabblePage.tsx
@@ -1,5 +1,65 @@
+import Video from "../../assets/videos/scrabble-presentation-web.webm";
+import Poster from "../../assets/images/scrabble-poster.png";
+import ScrabbleData from "../../data/scrabble_rules.json";
+import AboutData from "../../data/about_data.json";
+import { Link } from "react-router-dom";
+
 const ScrabblePage = () => {
-  return <div>ScrabblePage</div>;
+  return (
+    <div>
+      <section className="max-w-6xl mx-auto p-4">
+        <video
+          muted
+          autoPlay={false}
+          poster={Poster}
+          className="w-full h-full aspect-video rounded-lg"
+          controls
+        >
+          <source src={Video} type="video/webm" />
+        </video>
+      </section>
+      <section className=" max-w-7xl mx-auto px-8 py-8">
+        {ScrabbleData.data.map((info) => (
+          <div className="" key={info.id}>
+            <h2 className="text-blue-500 font-medium ">{info.title}</h2>
+            <p className="my-4 text-slate-500 bg-slate-100 rounded-lg p-4 text-justify">
+              {info.rule}
+            </p>
+            <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 my-8">
+              {info.images?.map((image, index) => (
+                <img
+                  src={image.url}
+                  alt="image"
+                  key={index}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+      </section>
+      <section className="max-w-5xl mx-auto p-8 rounded-xl mb-16 border-2 border-yellow-400">
+        {AboutData.details.map((detail) => (
+          <div className="shadow-md p-4 rounded-xl mb-4" key={detail.id}>
+            <h3 className="text-slate-500 font-medium mb-4 ">{detail.title}</h3>
+            <p className="bg-slate-100 p-4 rounded-b-xl">{detail.content}</p>
+            {detail.title.toLowerCase() === "nous contacter" ? (
+              <div className="flex items-center justify-center">
+                <Link
+                  className="text-blue-500 hover:text-yellow-500  transition-colors max-w-xs my-4 text-sm underline underline-offset-2 mx-auto"
+                  to={`/contact`}
+                >
+                  Contactez-nous Maintenant
+                </Link>
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default ScrabblePage;
